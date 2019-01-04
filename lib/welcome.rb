849084bd6e9297d0aee5b238ace2cbbb1f9085af
Tom Holmes <tholmes59@gmail.com>
2019-01-04 04:51:37 +0000

    Automatically backed up by Learn

--- lib/welcome.rb
+++ lib/welcome.rb
@@ -1,4 +1,4 @@
-filter_parameter_logging# Edit this file to output "Welcome to Tic Tac Toe!"
+# Edit this file to output "Welcome to Tic Tac Toe!"
 # You can see what this file does by running:
 # ruby lib/welcome.rb from your terminal.
 puts "Welcome to Tic Tac Toe!"
